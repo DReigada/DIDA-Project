134a343e429f6dcfe5f0679b0fdcf1d20e266ecf
Daniel Reigada <daniel.reigada@gmail.com>
2017-11-04 14:26:32 +0000

    Add basic client implementation

---
+++ OGP_PacMan_Client/Client/ClientImpl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ClientServerInterface.Client;
+using ClientServerInterface.PacMan.Client;
+using ClientServerInterface.PacMan.Client.Game;
+using OGPPacManClient.Interface;
+
+namespace OGPPacManClient.Client {
+    internal class ClientImpl : IPacManClient {
+        private readonly BoardController controller;
+        private List<ConnectedClient> connectedClients;
+
+        public ClientImpl(BoardController controller) {
+            this.controller = controller;
+        }
+
+        public void UpdateState(Board board) {
+            controller.Update(board);
+        }
+
+        public void UpdateConnectedClients(List<ConnectedClient> clients) {
+            connectedClients = clients;
+        }
+    }
+}
